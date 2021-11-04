fdfd1c148c2f6be39c9da535ea8fcfc7a943c6e5
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-11-04 12:35:16 -0700

    Complete fill and mult asm files

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// start counter at 0
+@i
+M=0
+// loop resets after 8000 iterations
+@ceiling
+@8192
+D=A
+@ceiling
+M=D
+
+(LOOP)
+  // if (i > ceiling) set i to 0
+  @i
+  D=M
+  @ceiling
+  D=D-M
+  @0
+  D;JGT
+  // make RAM register for screen -1 so it is black if key pressed otherwise
+  // white
+
+  @KBD
+  D=M
+  @BLACK
+  D;JNE
+  
+  // make RAM register 0 (white)
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+  @inc
+  0;JMP
+
+  // make ram register -1 (black)
+  (BLACK)
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+
+  // increment i 
+  (inc)
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
