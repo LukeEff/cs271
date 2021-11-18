97827c405674ffa8b16f3445a3b89edfe4570f9e
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-11-18 15:02:42 -0800

    Add error function to error.c

---
+++ cploration/c08/error.c
@@ -0,0 +1,21 @@
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *error_messages[] = 
+{
+  [EXIT_INCORRECT_ARGUMENTS] = "Usage: %s [filename]",
+};
+
+void exit_program(enum exitcode code, ...)
+{
+  va_list arguments;
+  va_start(arguments, code);
+
+  printf("ERROR: ");
+  vfprintf(stdout, error_messages[code], arguments);
+  printf("\n");
+
+  va_end(arguments);
+  exit(code);
+}
