055a6c37864b054a1c317a1a9b38f22544b338be
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-11-18 15:08:05 -0800

    Add errors to main.c
    Add new errors that main.c uses

--- cploration/c08/error.h
+++ cploration/c08/error.h
@@ -6,7 +6,7 @@
 /*
  * Error code 1 represents that given file does not exist.
  */
-enum exitcode { EXIT_INCORRECT_ARGUMENTS = 1 };
+enum exitcode { EXIT_INCORRECT_ARGUMENTS = 1, EXIT_CANNOT_OPEN_FILE = 2 };
 
 void exit_program(enum exitcode code, ...);
 
