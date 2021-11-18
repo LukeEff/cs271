2cf364e1c9603adef434a164d8fce5e596f4904b
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-11-18 14:56:10 -0800

    Add enumeration exit code to error.h

--- cploration/c08/error.h
+++ cploration/c08/error.h
@@ -1,4 +1,13 @@
 #ifndef __ERROR_H__
 #define __ERROR_H__
 
+#include <stdarg.h>
+
+/*
+ * Error code 1 represents that given file does not exist.
+ */
+enum exitcode { EXIT_INCORRECT_ARGUMENTS = 1 };
+
+void exit_program(enum exitcode code, ...);
+
 #endif
