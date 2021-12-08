aa6a867d8a746f3bf39f61408db4852e4fe175d3
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2021-12-08 15:52:34 -0800

    Copy files from cploration 10 to project-6

---
+++ 06/main.c
@@ -0,0 +1,26 @@
+#include "parser.h"
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h> 
+
+#define MAX_INSTRUCTION_COUNT 30000
+
+int main(int argc, const char *argv[]) {
+
+  instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
+
+  if (argc != 2) {
+    exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]); 
+  }
+
+  FILE *fin = fopen(argv[1], "r");
+   
+  if (fin == NULL) {
+    exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
+  }
+
+  int num_instructions = parse(fin, instructions);
+  fclose(fin);
+  free(instructions);
+  return 0;
+}
